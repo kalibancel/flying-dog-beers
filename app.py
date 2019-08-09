9ab030ca3af95c2ce079b44d55af17743e495809
kalibancel <53943621+kalibancel@users.noreply.github.com>
2019-08-09 10:38:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,32 +4,38 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
 
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name='IBU',
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name='ABV',
-    marker={'color':color2}
-)
+companies = ['Fannie', 'Freddie', 'FHLB', 'Farm Credit'];
+debt = [10, 20, 30, 40];
+fig = go.Pie(labels=companies, values=debt,
+                        marker={'colors': ['#EF963B', '#C93277', '#349600', '#EF533B']}, textinfo='label')]
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = 'Beer Comparison'
-)
+#beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+#ibu_values=[35, 60, 85, 75]
+#abv_values=[5.4, 7.1, 9.2, 4.3]
+#color1='lightblue'
+#color2='darkgreen'
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+#bitterness = go.Bar(
+#    x=beers,
+#    y=ibu_values,
+#    name='IBU',
+#    marker={'color':color1}
+#)
+#alcohol = go.Bar(
+#    x=beers,
+#    y=abv_values,
+#    name='ABV',
+#    marker={'color':color2}
+#)
+
+#beer_data = [bitterness, alcohol]
+#beer_layout = go.Layout(
+#    barmode='group',
+#    title = 'Beer Comparison'
+#)
+
+#beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 ########### Display the chart
 
@@ -40,7 +46,7 @@ app.layout = html.Div(children=[
     html.H1('Flying Dog Beers'),
     dcc.Graph(
         id='flyingdog',
-        figure=beer_fig
+        figure=fig
     ),
     html.A('Code on Github', href='https://github.com/austinlasseter/flying-dog-beers'),
     html.Br(),
